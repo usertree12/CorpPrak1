5d4fcf1b69797603a9a1e32cb5733d5edae37fd6
usertree12 <Maorm22@outlook.com>
2025-05-19 15:10:02 +0300

    Готовое

---
+++ Prak1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace prak1
+{
+    public class PRAK1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите путь к файлу: ");
+            string path = Console.ReadLine()!;
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                Console.WriteLine("Путь не может быть пустым");
+                return;
+            }
+            Console.Write("Введите слово, которое надо найти: ");
+
+            string word = Console.ReadLine()!;
+
+            if (string.IsNullOrWhiteSpace(word))
+            {
+                Console.WriteLine("Это поле не может быть пустым");
+                return;
+            }
+
+            word = word.ToLower();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string line;
+                    int count = 0;
+
+                    while ((line = sr.ReadLine()!) != null)
+                    {
+
+                        string lowerline = line.ToLower();
+
+                        string[] words = lowerline.Split(new char[] { ' ', ',', '.', '!', '?', ':', ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                        foreach (var w in words)
+                        {
+                            if (w == word)
+                            {
+                                count++;
+                            }
+                        }
+
+                    }
+
+
+                    Console.WriteLine($"Слово '{word}' найдено в файле {count} раз");
+                }
+            }
+
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Файл не найден");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+            }
+
+        }
+    }
+}
